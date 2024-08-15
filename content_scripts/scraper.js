0f5d86b79976392e08feb62ca559531f72402ba5
sackidude <nils.nilsen9@outlook.com>
2024-08-15 20:51:46 +0200

    refactor and drop unibet support

--- content_scripts/scraper.js
+++ content_scripts/scraper.js
@@ -1,6 +1,6 @@
 (function () {
   var data = new Map();
-  console.log("Executing scraper_content.js");
+  console.info("Executing scraper_content.js");
   /**
    * Check and set a global guard variable.
    * If this content script is injected into the same page again,
@@ -11,97 +11,121 @@
   }
   window.hasRun = true;
 
-  function bet365() {
-    console.log("Fetching bet365");
+  browser.runtime.onMessage.addListener((message) => {
+    console.info("Scraping...");
+
+    if (message != "scrape") {
+      console.error("Invalid message");
+      return;
+    }
+    const url = new URL(document.URL);
+    if (url.hostname != "www.bet365.com") {
+      console.error("Invalid page");
+      throw new Error("Wrong page");
+    }
+
     try {
       const navButtons = document.getElementsByClassName(
         "sph-MarketGroupNavBarButton_Content"
       );
-      const lag1 = document.getElementsByClassName(
-        "sph-FixturePodHeader_TeamName"
-      )[0].innerHTML;
-      const lag2 = document.getElementsByClassName(
-        "sph-FixturePodHeader_TeamName"
-      )[0].innerHTML;
+      console.assert(navButtons.length == 2) // The least number of categories needed.
 
-      // Main page
-      let fulltimeDoubleChance = document.getElementsByClassName(
-        "srb-ParticipantResponsiveText_Odds"
-      );
-      data.set("Fulltid > 1", fulltimeDoubleChance[0].innerHTML);
-      data.set("Fulltid > X", fulltimeDoubleChance[1].innerHTML);
-      data.set("Fulltid > 2", fulltimeDoubleChance[2].innerHTML);
-      data.set("Dubbelchans > 1X", fulltimeDoubleChance[3].innerHTML);
-      data.set("Dubbelchans > X2", fulltimeDoubleChance[4].innerHTML);
-      data.set("Dubbelchans > 12", fulltimeDoubleChance[5].innerHTML);
-      let overUnder = document.getElementsByClassName(
-        "gl-ParticipantOddsOnly_Odds"
-      );
-      data.set("Totalt antal mål > Över", overUnder[0].innerHTML);
-      data.set("Totalt antal mål > Under", overUnder[1].innerHTML);
-      data.set(
-        lag1 + " vinner och båda lagen gör mål > Ja",
-        overUnder[2].innerHTML
-      );
-      data.set(
-        lag2 + " vinner och båda lagen gör mål > Ja",
-        overUnder[3].innerHTML
-      );
-      data.set("Oavgjort och båda lagen gör mål > Ja", overUnder[4].innerHTML);
-      data.set(
-        lag1 + " vinner och båda lagen gör mål > Nej",
-        overUnder[5].innerHTML
-      );
-      data.set(
-        lag2 + " vinner och båda lagen gör mål > Nej",
-        overUnder[6].innerHTML
-      );
-      data.set("Oavgjort och båda lagen gör mål > Nej", overUnder[8].innerHTML);
+      const [leftTeam, rightTeam] = getTeamNames();
 
-      // Asian
-      navButtons[2].click();
+      // Currently on popular tab
+      fetchPopular(data, leftTeam, rightTeam);
 
-      // Save and show data
-      document.body.innerHTML = "Erbjudande\tBet365\n";
-      let sorted = new Map([...data.entries()].sort());
+      navButtons[1].click(); // Bet builder
+      fetchBetBuilder(data, leftTeam, rightTeam);
 
-      for (let [key, value] of sorted) {
-        document.body.innerHTML += key + "\t" + value + "\n";
-      }
+      navButtons[2].click(); // Asian
+      fetchAsian(data, leftTeam, rightTeam);
 
-      console.log(data);
+      saveAndShowData(data);
     } catch (error) {
       console.error("Possibly invalid page: " + error);
     }
-  }
-
-  function unibet() {
-    console.log("Fetching unibet");
-    try {
-      const test = document.getElementsByClassName(
-        "seh-ExclusionWrapper_ExclusionText"
-      )[0].innerHTML;
-      console.log(test);
-    } catch (error) {
-      console.error("Invalid page");
-    }
-  }
-
-  /**
-   * Listen for messages from the background script.
-   * Call "bet365()" or "unibet()".
-   */
-  browser.runtime.onMessage.addListener((message) => {
-    switch (message.command) {
-      case "Bet365":
-        bet365();
-        break;
-      case "Unibet":
-        unibet();
-        break;
-      default:
-        console.error("Invalid message from background script");
-        break;
-    }
   });
 })();
+
+function getTeamNames() {
+  const leftTeam = document.getElementsByClassName(
+    "sph-FixturePodHeader_TeamName"
+  )[0].innerHTML;
+  const rightTeam = document.getElementsByClassName(
+    "sph-FixturePodHeader_TeamName"
+  )[0].innerHTML;
+  return [leftTeam, rightTeam];
+}
+
+function fetchPopular(data, leftTeam, rightTeam) {
+  let fulltimeDoubleChance = document.getElementsByClassName(
+    "srb-ParticipantResponsiveText_Odds"
+  );
+  data.set("Fulltid > 1", fulltimeDoubleChance[0].innerHTML);
+  data.set("Fulltid > X", fulltimeDoubleChance[1].innerHTML);
+  data.set("Fulltid > 2", fulltimeDoubleChance[2].innerHTML);
+  data.set("Dubbelchans > 1X", fulltimeDoubleChance[3].innerHTML);
+  data.set("Dubbelchans > X2", fulltimeDoubleChance[4].innerHTML);
+  data.set("Dubbelchans > 12", fulltimeDoubleChance[5].innerHTML);
+  let overUnder = document.getElementsByClassName(
+    "gl-ParticipantOddsOnly_Odds"
+  );
+  data.set("Totalt antal mål > Över", overUnder[0].innerHTML);
+  data.set("Totalt antal mål > Under", overUnder[1].innerHTML);
+  data.set(
+    leftTeam + " vinner och båda lagen gör mål > Ja",
+    overUnder[2].innerHTML
+  );
+  data.set(
+    rightTeam + " vinner och båda lagen gör mål > Ja",
+    overUnder[3].innerHTML
+  );
+  data.set("Oavgjort och båda lagen gör mål > Ja", overUnder[4].innerHTML);
+  data.set(
+    leftTeam + " vinner och båda lagen gör mål > Nej",
+    overUnder[5].innerHTML
+  );
+  data.set(
+    rightTeam + " vinner och båda lagen gör mål > Nej",
+    overUnder[6].innerHTML
+  );
+  data.set("Oavgjort och båda lagen gör mål > Nej", overUnder[8].innerHTML);
+}
+
+function fetchBetBuilder(data, leftTeam, rightTeam) {
+
+}
+
+function fetchAsian(data, leftTeam, rightTeam) {
+
+}
+
+function saveAndShowData(data) {
+  document.body.innerHTML = "Erbjudande\tBet365\n";
+  let sorted = new Map([...data.entries()].sort());
+
+  for (let [key, value] of sorted) {
+    document.body.innerHTML += key + "\t" + value + "\n";
+  }
+
+  console.debug(data);
+}
+
+function selectMarketGroup(name) {
+  // Select all divs with class bbw-BetBuilderEmbeddedMarketGroup
+  const divs = document.querySelectorAll('div.bbw-BetBuilderEmbeddedMarketGroup');
+
+  // Loop through each div and check the condition
+  let targetDiv = null;
+  divs.forEach(div => {
+    const firstChild = div.firstElementChild;
+    if (firstChild) {
+      const firstChildOfFirstChild = firstChild.firstElementChild;
+      if (firstChildOfFirstChild && firstChildOfFirstChild.innerHTML.trim() === name) {
+        targetDiv = div;
+      }
+    }
+  });
+  return targetDiv;
+}
